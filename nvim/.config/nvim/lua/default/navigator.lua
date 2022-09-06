9b4a77f5e00fc929a8e752453468c1dfe8ece776
hexdhog <hexdhog@gmail.com>
2022-09-06 18:33:53 +0200

    fixed navigator conflicting command

--- nvim/.config/nvim/lua/default/navigator.lua
+++ nvim/.config/nvim/lua/default/navigator.lua
@@ -3,5 +3,8 @@ require "navigator".setup({
 	transparency = 15,
 	lsp = {
 		format_on_save = false
+	},
+	keymaps = {
+		{ key = '<Space>gla', mode = 'n', func = require('navigator.codelens').run_action, desc = 'run code lens action' }
 	}
 })
