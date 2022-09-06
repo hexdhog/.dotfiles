9910a92391707e7f245e84f53d811d2005918cc9
hexdhog <hexdhog@gmail.com>
2022-09-06 15:12:06 +0200

    added navigator setup

--- nvim/.config/nvim/lua/default/autopairs.lua
+++ nvim/.config/nvim/lua/default/autopairs.lua
@@ -11,7 +11,7 @@ npairs.setup {
 		javascript = { "string", "template_string" },
 		java = false,
 	},
-	disable_filetype = { "TelescopePrompt", "spectre_panel" },
+	disable_filetype = { "TelescopePrompt", "spectre_panel", "guihua", "guihua_rust", "clap_input" },
 	fast_wrap = {
 		map = "<M-e>",
 		chars = { "{", "[", "(", '"', "'" },
