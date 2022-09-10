8a623c9e8ab64e717080758a6103611499d4c4ba
hexdhog <hexdhog@gmail.com>
2022-09-10 16:13:21 +0200

    added clangd lsp config

--- nvim/.config/nvim/lua/default/lsp/init.lua
+++ nvim/.config/nvim/lua/default/lsp/init.lua
@@ -1,4 +1,4 @@
-local status, _ = pcall(require, "lspconfig")
+local status, lspconfig = pcall(require, "lspconfig")
 if not status then
 	print("ERROR: module 'lspconfig' not found")
 	return
@@ -7,3 +7,24 @@ end
 require("mason").setup()
 require("mason-lspconfig").setup()
 require("default.lsp.handlers").setup()
+
+lspconfig.clangd.setup {
+	cmd = {
+		"clangd",
+		"--all-scopes-completion",
+		"--suggest-missing-includes",
+		"--background-index",
+		"--pch-storage=disk",
+		"--cross-file-rename",
+		"--log=info",
+		"--completion-style=detailed",
+		"--enable-config", -- clangd 11+ supports reading from .clangd configuration file
+		"--clang-tidy",
+		"--offset-encoding=utf-16", --temporary fix for null-ls
+		-- "--clang-tidy-checks=-*,llvm-*,clang-analyzer-*,modernize-*,-modernize-use-trailing-return-type",
+		-- "--fallback-style=Google",
+		-- "--header-insertion=never",
+		-- "--query-driver=<list-of-white-listed-complers>"
+	},
+	filetypes = {"c", "cpp", "objc", "objcpp"},
+}
