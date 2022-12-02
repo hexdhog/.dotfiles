bb3e9b74e9d7c629f2ca7b3756447f42a0ebe1d1
hexdhog <hexdhog@gmail.com>
2022-12-02 19:48:35 +0100

    nvim - added telescope lsp integration

--- nvim/.config/nvim/lua/plugin/config/lspconfig.lua
+++ nvim/.config/nvim/lua/plugin/config/lspconfig.lua
@@ -7,12 +7,11 @@ end
 lspconfig.pyright.setup({
 	on_attach = function()
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
-		vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
-		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = 0 })
+		-- vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
+		-- vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
+		-- vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = 0 })
 		vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { buffer = 0 })
 		vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, { buffer = 0 })
 		vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, { buffer = 0 })
-		vim.keymap.set("n", "<leader>dd", "<cmd>Telescope diagnostics<CR>", { buffer = 0 })
 	end
 })
